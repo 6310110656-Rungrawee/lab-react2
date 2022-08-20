efee9f216a0591729311cb7a3a06f67faa29b3a1
RUNGRAWEE SAE-CHIN <6310110656@psu.ac.th>
2022-08-21 01:35:07 +0700

    part7-5

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
-import React, {useEffect, useState, useRef} from 'react';
+import React, { useEffect, useState, useRef } from 'react';
+import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  Button,
+  DialogTitle
+} from "@material-ui/core";
 import CharacterCard from './CharacterCard';
-import './App.css';
+import './App.scss';
 
 const imagesArray = [
   {
@@ -31,9 +39,9 @@ const imagesArray = [
 
 function shuffleCards(array) {
   const length = array.length;
-  for (let i=length; i>0 ; i--) {
-    const randomIndex = Math.floor(Math.random()*i);
-    const currentIndex = i-1;
+  for (let i = length; i > 0; i--) {
+    const randomIndex = Math.floor(Math.random() * i);
+    const currentIndex = i - 1;
     const temp = array[currentIndex];
     array[currentIndex] = array[randomIndex];
     array[randomIndex] = temp;
@@ -42,45 +50,158 @@ function shuffleCards(array) {
 }
 
 export default function App() {
-  const [cards, setCards]=useState (
+  const [cards, setCards] = useState(
     shuffleCards.bind(null, imagesArray.concat(imagesArray))
   );
 
-}
+  const [openCards, setOpenCards] = useState([]);
+  const [clearedCards, setClearedCards] = useState({});
+  const [shouldDisableAllCards, setShouldDisableAllCards] = useState(false);
+  const [moves, setMoves] = useState(0);
+  const [showModal, setShowModal] = useState(false);
+  const [bestScore, setBestScore] = useState(
+    JSON.parse(localStorage.getItem("bestScore")) || Number.POSITIVE_INFINITY
+  );
+  const timeout = useRef(null);
 
-return (
-  <div className="App">
-    <header>
-      <h3>Play the Flip card game</h3>
-      <div>Select two cards with same content consequtively to make them vanish</div>
-    </header>
+  const disable = () => {
+    setShouldDisableAllCards(true);
+  };
+  const enable = () => {
+    setShouldDisableAllCards(false);
+  };
 
-    <div className="container">
-      {cards.map((card, index) => {
-        return (
-          <Card key = {index}
-          card = {card}
-          index = {index}
-          isDisabled = {shouldDisableAllCards}
-          isInactive = {checkIsInactive(card)}
-          isFlipped = {chackIsFlipped(index)}
-          onClock = {handleCardClick}
-          />
-        );
-      })}
-    </div>
+  const checkCompletion = () => {
+    if (Object.keys(clearedCards).length === imagesArray.length) {
+      setShowModal(true);
+      const highScore = Math.min(moves, bestScore);
+      setBestScore(highScore);
+      localStorage.setItem("bestScore", highScore);
+    }
+  };
 
-    <footer>
-      <div className = "score">
-        <div className = "move">
-          <span className ="bold">Move : </span> {move}
-        </div>
-        {localStorage.getItem("bestScore") && (
-          <div className = "high-score">
-            <span className = "bold">Best Score : </span> {bestScore}
-          </div>
-        )}
+  const evaluate = () => {
+    const [first, second] = openCards;
+    enable();
+    if (cards[first].type === cards[second].type) {
+      setClearedCards((prev) => ({ ...prev, [cards[first].type]: true }));
+      setOpenCards([]);
+      return;
+    }
+
+    timeout.current = setTimeout(() => {
+      setOpenCards([]);
+    }, 500);
+  };
+
+  const handleCardClick = (index) => {
+    if (openCards.length === 1) {
+      setOpenCards((prev) => [...prev, index]);
+      setMoves((moves) => moves + 1);
+      disable();
+    } else {
+      clearTimeout(timeout.current);
+      setOpenCards([index]);
+    }
+  };
+
+  useEffect(() => {
+    let timeout = null;
+    if (openCards.length === 2) {
+      timeout = setTimeout(evaluate, 300);
+    }
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [openCards]);
+
+  useEffect(() => {
+    checkCompletion();
+  }, [clearedCards]);
+  const checkIsFlipped = (index) => {
+    return openCards.includes(index);
+  };
+
+  const checkIsInactive = (card) => {
+    return Boolean(clearedCards[card.type]);
+  };
+
+  const handleRestart = () => {
+    setClearedCards({});
+    setOpenCards([]);
+    setShowModal(false);
+    setMoves(0);
+    setShouldDisableAllCards(false);
+    setCards(shuffleCards(imagesArray.concat(imagesArray)));
+  };
+
+
+
+  return (
+    <div className="App">
+      <header>
+        <h3>Play the Flip card game</h3>
+        <div>Select two cards with same content consequtively to make them vanish</div>
+      </header>
+
+      <div className="container">
+        {cards.map((card, index) => {
+          return (
+            <Card key={index}
+              card={card}
+              index={index}
+              isDisabled={shouldDisableAllCards}
+              isInactive={checkIsInactive(card)}
+              isFlipped={chackIsFlipped(index)}
+              onClock={handleCardClick}
+            />
+          );
+        })}
       </div>
-    </footer>
-  </div>
-) 
+
+      <footer>
+        <div className="score">
+          <div className="moves">
+            <span className="bold">Moves : </span> {moves}
+          </div>
+          {localStorage.getItem("bestScore") && (
+            <div className="high-score">
+              <span className="bold">Best Score : </span> {bestScore}
+            </div>
+          )}
+        </div>
+
+        <div className="Restart">
+          <Buttom onClock={handleReStart} color="primary" variant="contained">
+            Restart
+          </Buttom>
+        </div>
+
+      </footer>
+
+
+      <Dialog open={showModal}
+        disableBackdropClick
+        disableEscapeKwyDown
+        aria-labelledby="alert-dialog-title"
+        aria-describeadby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          Yeah!! You win!
+        </DialogTitle>
+
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            You completed the game in {moves} moves. Your best score is{" "} {bestScore} moves.
+          </DialogContentText>
+        </DialogContent>
+
+        <DialogActions>
+          <Button onClick={handleRestart} color="primary">
+            Restart
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+} 
--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -1,25 +1,28 @@
 import React from 'react';
 import classnames from "classnames";
 import './card.scss';
-const Card = ({ onclick, card, index, isInactive, isFlipped, isDisabled }) => {
+const Card = ({onclick, card, index, isInactive, isFlipped, isDisabled}) => {
     const handleClick = () => {
         !isFlipped && !isDisabled && onclick(index);
     };
-    return (
-        <div className={classnames("card", {
-            "is-flipped": isFlipped,
-            "is-inactive": isInactive
+    return(
+        <div className = {classnames("card",{
+                "is-flipped": isFlipped,
+                "is-inactive": isInactive
         })}
-            onclick={handleClick}
+        onclick = {handleClick}
         >
-            <div className="card-face card-front-face">
-                <img src="https://drive.google.com/file/d/1muVEPIxjfvHI12b3oGJ2iGAwbMhFp-Dn/view?usp=sharing"
-                    alt="cover" />
+            <div className = "card-face card-front-face">
+                <img src = "https://drive.google.com/file/d/1l7qtdnIr8N_klr3doY2TdUE0v9Z7y9Qm/view?usp=sharing"
+                alt = "cover"/>
             </div>
-            <div className="card-face card-back-face">
-                <img src=""
-                    alt="cover" />
+
+            <div className = "card-face card-back-face">
+                <img src = "https://drive.google.com/file/d/1I5CY8WI7BvpIlegn3Ns1ho3ezIqoAglH/view?usp=sharing"
+                alt = "cover"/>
             </div>
         </div>
     );
-};
+}; 
+
+export default Card; 
