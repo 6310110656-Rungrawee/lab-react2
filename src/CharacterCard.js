80cd3294ef1bed3933e6d2d479c72dd5e3e07ffa
RUNGRAWEE SAE-CHIN <6310110656@psu.ac.th>
2022-08-21 00:51:09 +0700

    part7-2

--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -1,25 +1,25 @@
-import React, { useState, useEffect, useRef } from 'react';
-
-export default function CharacterCard(props){
-    const [active, setActive] = useState(false);
-    const attemptRef = useRef(props.attempt);
-
-    const activate = () => {
-        if(!active){
-        setActive(true)
-        props.activationHandler(props.value)
-    }
-}
-
-    useEffect(()=>{
-        if(attemptRef.current != props.attempt){
-            setActive(false)
-            attemptRef.current = props.attempt
-            }           
-    })
-    const className = `card ${active ? 'activeCard': ''}`
-
+import React from 'react';
+import classnames from "classnames";
+import './card.scss';
+const Card = ({ onclick, card, index, isInactive, isFlipped, isDisabled }) => {
+    const handleClick = () => {
+        !isFlipped && !isDisabled && onclick(index);
+    };
     return (
-        <div className={className} onClick={activate}>{props.value}</div>
-    )
-}
+        <div className={classnames("card", {
+            "is-flipped": isFlipped,
+            "is-inactive": isInactive
+        })}
+            onclick={handleClick}
+        >
+            <div className="card-face card-front-face">
+                <img src="https://drive.google.com/file/d/1muVEPIxjfvHI12b3oGJ2iGAwbMhFp-Dn/view?usp=sharing"
+                    alt="cover" />
+            </div>
+            <div className="card-face card-back-face">
+                <img src=""
+                    alt="cover" />
+            </div>
+        </div>
+    );
+};
--- src/WordCard.js
+++ src/WordCard.js
@@ -1,4 +1,5 @@
-import React, {useState} from 'react';
+import React from 'react';
+import React, { useState } from 'react';
 import _, { attempt } from 'lodash';
 import CharacterCard from './CharacterCard';
 
@@ -6,44 +7,47 @@ const prepareStateFromWord = (given_word) => {
     let word = given_word.toUpperCase()
     let chars = _.shuffle(Array.from(word))
     return {
-    word,
-    chars,
-    attempt: 1,
-    guess: '',
-    completed: false
+        word,
+        chars,
+        attempt: 1,
+        guess: '',
+        completed: false
     }
 }
 
-export default function WordCard(props){
+export default function WordCard(props) {
 
+    const activationHandler = c => {
+        console.log(`${c} has been activated.`)
+    }
     const [state, setState] = useState(prepareStateFromWord(props.value))
 
     const activationHandler = (c) => {
-        console.log(`${c} OK.`)
+        console.log(`${c} has been activated.`)
 
         let guess = state.guess + c
-        setState({...state, guess})
+        setState({ ...state, guess })
 
-        if(guess.length == state.word.length){
-            if(guess == state.word){
-                console.log('GOOD JOB')
-                setState({...state, guess: '', completed: true})
-        }
+        if (guess.length == state.word.length) {
+            if (guess == state.word) {
+                console.log('yeah!')
+                setState({ ...state, guess: '', completed: true })
+            }
 
-        else{
-            console.log('WRONG')
-            setState({...state, guess: '', attempt: state.attempt + 1})
+            else {
+                console.log('reset next attempt')
+                setState({ ...state, guess: '', attempt: state.attempt + 1 })
+            }
         }
     }
-}
 
     return (
         <div>
-            { 
-                state.chars.map((c, i) => 
-                <CharacterCard value={c} key={i} activationHandler={activationHandler} attempt={state.attempt}/>
+            {
+                Array.from(props.value).map((c, i) =>
+                    <CharacterCard value={c} key={i} activationHandler={activationHandler} attempt={state.attempt} />
                 )
-        }
+            }
         </div>
     )
 }
